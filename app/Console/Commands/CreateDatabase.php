9d968a2552d42edc7f0b8d9814c27e7b6f908924
mohammed-adil-moughal <mohammedadil755@gmail.com>
2021-09-05 12:21:18 +0300

    Added console command for Creating DB

---
+++ app/Console/Commands/CreateDatabase.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use PDO;
+use PDOException;
+
+class CreateDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a Database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $database = env('DB_DATABASE', false);
+
+        if (! $database) {
+            $this->info('DB Name param missing');
+            return;
+        }
+
+        try {
+            $pdo = $this->getPDOConnection(env('DB_HOST'), env('DB_PORT'), env('DB_USERNAME'), env('DB_PASSWORD'));
+            $pdo->exec(sprintf('CREATE DATABASE IF NOT EXISTS %s;', $database));
+            $this->info('Created database');
+        } catch (PDOException $exception) {
+            $this->error(sprintf('Failed to create %s database, %s', $database, $exception->getMessage()));
+        }
+
+    }
+
+    /**
+     * @param  string $host
+     * @param  integer $port
+     * @param  string $username
+     * @param  string $password
+     * @return PDO
+     */
+    private function getPDOConnection($host, $port, $username, $password)
+    {
+        return new PDO(sprintf('mysql:host=%s;port=%d;', $host, $port), $username, $password);
+    }
+}
